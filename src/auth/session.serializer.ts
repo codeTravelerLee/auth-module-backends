4308b64bf1e1ddd5c264ddbc06a515887bdb335d
codeTravelerLee <12jack030220@gmail.com>
2025-02-05 11:28:36 +0000

    스트래티지 구현

--- src/auth/session.serializer.ts
+++ src/auth/session.serializer.ts
@@ -1,14 +1,29 @@
-import { Injectable } from "@nestjs/common";
-import { PassportSerializer } from "@nestjs/passport";
-import { UserService } from "src/user/user.service";
+import { Injectable } from '@nestjs/common';
+import { PassportSerializer } from '@nestjs/passport';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  constructor(pirvate userService: UserService){
+  constructor(private userService: UserService) {
     super();
   }
 
-  serializeUser(user:any, done: (err: Error, user: any) => void) : any {
-    done(null, user.email)
+  serializeUser(user: any, done: (err: Error | null, user: any) => void): any {
+    done(null, user.email);
+  }
+
+  async deserializeUser(
+    payload: any,
+    done: (err: Error | null, payload: any) => void,
+  ): Promise<any> {
+    const user = await this.userService.getUser(payload);
+
+    if (!user) {
+      done(new Error('No user'), null);
+      return;
+    }
+
+    const { password, ...userInfo } = user;
+    done(null, userInfo);
   }
 }
